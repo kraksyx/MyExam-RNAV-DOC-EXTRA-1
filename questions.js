5bf207502024cf35e7f87f4e843cdb5a12b4822a
kraksyx <blahblahusdme@gmail.com>
2025-07-28 22:56:01 +0400

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,352 @@
+const questions = [
+  {
+    "question": "Wavelength is the distance of:",
+    "options": [
+      "Crest to crest",
+      "",
+      "",
+      ""
+    ],
+    "correct": 0
+  },
+  {
+    "question": "ILS glide path is 2.8\u00b0 and 1.5 NM:",
+    "options": [
+      "735 ft",
+      "",
+      "",
+      ""
+    ],
+    "correct": 0
+  },
+  {
+    "question": "With 255 radial and OBS set to 190, full deflection to left with \"FROM\" showing:",
+    "options": [
+      "You're west of the VOR and flying away from it.",
+      "",
+      "",
+      ""
+    ],
+    "correct": 0
+  },
+  {
+    "question": "Feeder lines are located at the following types:",
+    "options": [
+      "Coaxial cable up to 3000 MHz",
+      "Waveguide above 3000 MHz",
+      "",
+      ""
+    ],
+    "correct": 0
+  },
+  {
+    "question": "HF tuned to reflect from ionosphere max distance is:",
+    "options": [
+      "1500 NM",
+      "",
+      "",
+      ""
+    ],
+    "correct": 0
+  },
+  {
+    "question": "A long range NDB transmits on:",
+    "options": [
+      "200 kHz, A1A",
+      "",
+      "",
+      ""
+    ],
+    "correct": 0
+  },
+  {
+    "question": "Phase difference (figure):",
+    "options": [
+      "225",
+      "",
+      "",
+      ""
+    ],
+    "correct": 0
+  },
+  {
+    "question": "Centered indication on ADF means:",
+    "options": [
+      "Null",
+      "",
+      "",
+      ""
+    ],
+    "correct": 0
+  },
+  {
+    "question": "D layer:",
+    "options": [
+      "Appears at night",
+      "",
+      "",
+      ""
+    ],
+    "correct": 0
+  },
+  {
+    "question": "MLS without DME/P behaves like:",
+    "options": [
+      "ILS",
+      "",
+      "",
+      ""
+    ],
+    "correct": 0
+  },
+  {
+    "question": "Reflection from ionospheric layers uses:",
+    "options": [
+      "HF",
+      "",
+      "",
+      ""
+    ],
+    "correct": 0
+  },
+  {
+    "question": "MLS uses the following wavelength:",
+    "options": [
+      "Centimetric (SHF)",
+      "",
+      "",
+      ""
+    ],
+    "correct": 0
+  },
+  {
+    "question": "To make A1A audible:",
+    "options": [
+      "BFO ON",
+      "",
+      "",
+      ""
+    ],
+    "correct": 0
+  },
+  {
+    "question": "Abnormal long range in VDF may be caused by:",
+    "options": [
+      "Super refraction in the atmosphere",
+      "",
+      "",
+      ""
+    ],
+    "correct": 0
+  },
+  {
+    "question": "In ILS, the ident is:",
+    "options": [
+      "Morse code by localizer 1250 MHz",
+      "",
+      "",
+      ""
+    ],
+    "correct": 0
+  },
+  {
+    "question": "Which of the following is transmitted via navigation message of GPS SV (except PRN)?",
+    "options": [
+      "Health",
+      "Almanac",
+      "Clock error",
+      "PRN"
+    ],
+    "correct": 3
+  },
+  {
+    "question": "Introduction of RNAV:",
+    "options": [
+      "All answers are correct",
+      "",
+      "",
+      ""
+    ],
+    "correct": 0
+  },
+  {
+    "question": "4000 ft above aircraft - top of cloud (40 NM, BW = 4\u00b0, tilt = 3\u00b0):",
+    "options": [
+      "",
+      "",
+      "",
+      ""
+    ],
+    "correct": 0
+  },
+  {
+    "question": "Area Surveillance RADAR uses:",
+    "options": [
+      "600 MHz frequency",
+      "",
+      "",
+      ""
+    ],
+    "correct": 0
+  },
+  {
+    "question": "Which cannot be detected by AWR?",
+    "options": [
+      "Dry Hail",
+      "CAT",
+      "",
+      ""
+    ],
+    "correct": 1
+  },
+  {
+    "question": "Which statement is correct regarding Mode S SSR?",
+    "options": [
+      "Reduction of RT",
+      "",
+      "",
+      ""
+    ],
+    "correct": 0
+  },
+  {
+    "question": "Which error is corrected by DGPS?",
+    "options": [
+      "Clock error",
+      "Selective Availability",
+      "",
+      ""
+    ],
+    "correct": 0
+  },
+  {
+    "question": "AWR can be used on ground when:",
+    "options": [
+      "Clear of personnel and buildings",
+      "Cosecant beam selected",
+      "",
+      ""
+    ],
+    "correct": 0
+  },
+  {
+    "question": "Course line computer computes:",
+    "options": [
+      "Distance and direction to phantom waypoint",
+      "",
+      "",
+      ""
+    ],
+    "correct": 0
+  },
+  {
+    "question": "FMS provides:",
+    "options": [
+      "VNAV/LNAV planning",
+      "FMS position",
+      "",
+      ""
+    ],
+    "correct": 0
+  },
+  {
+    "question": "DGPS involves:",
+    "options": [
+      "Fixed ground stations providing corrections to GPS receiver",
+      "",
+      "",
+      ""
+    ],
+    "correct": 0
+  },
+  {
+    "question": "If DME indication is hidden on EFIS:",
+    "options": [
+      "Aircraft is out of DOC",
+      "",
+      "",
+      ""
+    ],
+    "correct": 0
+  },
+  {
+    "question": "FMS receives inputs mostly from:",
+    "options": [
+      "GPS",
+      "DME",
+      "",
+      ""
+    ],
+    "correct": 0
+  },
+  {
+    "question": "If an NDB has a published range of 30 NM, its accuracy is:",
+    "options": [
+      "Guaranteed signal during the day",
+      "",
+      "",
+      ""
+    ],
+    "correct": 0
+  },
+  {
+    "question": "An ADF uses a sense aerial to:",
+    "options": [
+      "Resolve ambiguous bearings",
+      "",
+      "",
+      ""
+    ],
+    "correct": 0
+  },
+  {
+    "question": "Marker Beacon frequency:",
+    "options": [
+      "VHF",
+      "",
+      "",
+      ""
+    ],
+    "correct": 0
+  },
+  {
+    "question": "ILS frequencies:",
+    "options": [
+      "VHF and UHF",
+      "",
+      "",
+      ""
+    ],
+    "correct": 0
+  },
+  {
+    "question": "An airplane\u2019s RMI shows an NDB bearing 070\u00b0, w/v calm. The airplane is to join a right-hand holding pattern at the NDB, the inbound leg of which is 330\u00b0. The airplane should:",
+    "options": [
+      "Fly to the NDB and join the pattern",
+      "",
+      "",
+      ""
+    ],
+    "correct": 0
+  },
+  {
+    "question": "VOR shows 075\u00b0 TO with CDI centered, NDB shows 012\u00b0 relative. Drift and heading?",
+    "options": [
+      "12\u00b0 Right, 063\u00b0",
+      "",
+      "",
+      ""
+    ],
+    "correct": 0
+  },
+  {
+    "question": "Maximum width of cone of silence above a VOR at 30,000 ft:",
+    "options": [
+      "11.6 NM",
+      "",
+      "",
+      ""
+    ],
+    "correct": 0
+  }
+];
